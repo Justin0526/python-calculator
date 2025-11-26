89da87af8e7dc7354c066d688a9b6969ef74790d
Justin Tang <jtang8446@gmail.com>
2025-11-26 17:20:26 +0800

    break test_add

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ from calculator import add, subtract, multiply, divide
 
 def test_add():
     """Test addition functinon"""
-    assert add(2, 3) == 5
+    assert add(2, 3) == 2
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
